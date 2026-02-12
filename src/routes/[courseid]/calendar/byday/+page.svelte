6e26b3e94f631e6f938b96eba86f763ca0a979a5
Eamonn de Leastar <edeleastar@wit.ie>
2026-02-12 07:38:01 +0000

    calendar routes

---
+++ src/routes/[courseid]/calendar/byday/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import CalendarComponent from "$lib/components/calendar/CalendarComponent.svelte";
+  import { page } from "$app/stores";
+
+  const courseId = $derived(($page.params.courseid as string) ?? "");
+</script>
+
+{#key courseId}
+  <CalendarComponent courseId={courseId} mode="day" />
+{/key}
