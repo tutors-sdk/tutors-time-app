6a6566f4bfa5da57350576b0937b6e51e784f7ed
Eamonn de Leastar <edeleastar@wit.ie>
2026-02-19 07:35:36 +0000

    initilisation of supabase in +page.ts

--- src/routes/[courseid]/calendar/byweek/+page.ts
+++ src/routes/[courseid]/calendar/byweek/+page.ts
@@ -1,6 +1,9 @@
-import { TutorsTime } from "$lib/tutors-time-service";
+import { PUBLIC_SUPABASE_ANON_KEY, PUBLIC_SUPABASE_URL } from "$env/static/public";
+import { initSupabase, TutorsTime } from "$lib/tutors-time-service";
 
 export async function load({ params }: { params: Record<string, string> }) {
+
+  initSupabase(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY);
   const courseId = (params.courseid ?? "").trim();
   if (!courseId) {
     return { course: null };
